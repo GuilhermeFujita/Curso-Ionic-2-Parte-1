2a93577d3fd3f3d16e1b9355ba093eaa8d34b92c
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-04-27 09:38:36 -0300

    Realizando requisições Http

--- aluraCar/src/pages/home/home.ts
+++ aluraCar/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-   constructor(public navCtrl: NavController) {
-   
+  public carros;
+
+   constructor(public navCtrl: NavController, private _http: Http) {
+
+     this._http
+     .get('https://aluracar.herokuapp.com')
+     .map(res => res.json())
+     .toPromise()
+     .then(carros => this.carros = carros);
   }
 }
