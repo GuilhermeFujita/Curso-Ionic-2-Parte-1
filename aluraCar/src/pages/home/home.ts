b7dcff8bb8aae69b38e2af87e0389e2831fa5c70
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-04-28 09:30:53 -0300

    Melhorando a experiência do usuário

--- aluraCar/src/pages/home/home.ts
+++ aluraCar/src/pages/home/home.ts
@@ -1,21 +1,45 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  public carros;
+	public carros;
 
-   constructor(public navCtrl: NavController, private _http: Http) {
+	constructor(public navCtrl: NavController,
+		private _http: Http,
+		private _loadingCtrl: LoadingController,
+		private _alertCtrl: AlertController) {}
 
-     this._http
-     .get('https://aluracar.herokuapp.com')
-     .map(res => res.json())
-     .toPromise()
-     .then(carros => this.carros = carros);
-  }
+	ngOnInit() {
+		let loader = this._loadingCtrl.create({
+			content: "Buscando novos carros. Aguarde"
+		}); // Criação do Loader
+
+		loader.present(); // Exibe o loader criado
+
+		this._http
+			.get('https://aluracar.herokuapp.com')
+			.map(res => res.json())
+			.toPromise()
+			.then(carros => {
+				this.carros = carros;
+				loader.dismiss(); //Fecha o Loader
+			})
+			 .catch(err => {
+				loader.dismiss();
+				console.log(err);
+
+				let alert = this._alertCtrl.create({
+					title: 'Falha na conexão',
+					buttons: [{ text: 'Estou ciente' }],
+					subTitle: 'Não foi possível obter a lista de carros. Tente mais tarde.'
+				});
+				alert.present();
+			});
+	}
 }
