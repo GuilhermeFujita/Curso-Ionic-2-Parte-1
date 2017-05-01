cbd8182aa5e50ec1173606fa36cf713212c625ca
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-05-01 10:40:41 -0300

    Preparando para enviar dados para a API

--- aluraCar/src/pages/escolha/escolha.ts
+++ aluraCar/src/pages/escolha/escolha.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, NavController } from 'ionic-angular';
 
 import { Acessorio } from '../../domain/carro/acessorio';
 import { Carro } from '../../domain/carro/carro';
+import { CadastroPage } from '../cadastro/cadastro';
 
 @Component({
     templateUrl: 'escolha.html'
@@ -14,7 +15,7 @@ export class EscolhaPage {
     public acessorios: Acessorio[];
     private _precoTotal: number;
 
-    constructor(public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.carro = navParams.get('carroSelecionado');
         this._precoTotal = this.carro.preco;
         this.acessorios = [
@@ -32,5 +33,12 @@ export class EscolhaPage {
         ligado?
         this._precoTotal += acessorio.preco :
         this._precoTotal -= acessorio.preco;
+    }
+
+    avancaNoAgendamento(){
+        this.navCtrl.push(CadastroPage, {
+            carro: this.carro,
+            precoTotal: this._precoTotal
+        })
     }
 }
